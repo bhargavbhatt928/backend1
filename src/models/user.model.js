12dba142ca4964ea4a5539e762f474435da44162
bhargavbhatt928 <bhargavbhatt928@gmail.com>
2024-07-24 11:17:16 +0530

    project setup complete

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -74,20 +74,25 @@ userSchema.pre("save", async function(next) {
 userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
 }
-userSchema.methods.generateAccessToken=function(){
-  return  jwt.sign(
+/**
+ * Generates a new access token for the user.
+ * @returns {string} The generated access token.
+ */
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
         {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullName
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-    )
+    );
 }
+
 userSchema.methods.generateRefreshToken=function(){
     return  jwt.sign(
         {
